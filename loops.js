182a82192d61d8d4ba86fe196c3dad2fabc49b9e
Nephtali Abisai Hernández Olvera <sistemas.naho13@gmail.com>
2019-12-13 10:48:02 -0700

    Add loops

---
+++ loops.js
@@ -0,0 +1,14 @@
+//if(a) { .. }
+//while(a) { .. }
+
+//while( a > 10 ) { .. }
+
+// for (a=5; a<10; a++) { console.log(a); }
+/* var a = 5;
+while( true ) { 
+    if(a>=10){ 
+        break; 
+    }
+    console.log(a);
+    a++;
+} */
